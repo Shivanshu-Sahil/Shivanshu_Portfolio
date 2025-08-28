e9f7faeda5b54ea05294049e122794faa5c5e111
Shivanshu <sahil060659@gmail.com>
2025-08-28 12:19:46 +0530

    deleted projects page

--- app/projects/page.tsx
+++
@@ -1,5 +0,0 @@
-import { projects } from '@/data/projects'
-import { ProjectCard } from '@/components/ui/ProjectCard'
-import { Navbar } from '@/components/layout/Navbar'
-
-// This file has been removed to disable the /projects route.
