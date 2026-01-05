cea7ccfd4387d8fff35de95fffee5f92a1ad42ee
Shivanshu <sahil060659@gmail.com>
2026-01-05 09:35:09 +0530

    chore: update resume Google Drive link

--- constants/routes.ts
+++ constants/routes.ts
@@ -18,7 +18,7 @@ export const ROUTES = {
     GITHUB: 'https://github.com/Shivanshu-Sahil',
     LINKEDIN: 'https://www.linkedin.com/in/shivanshu-sahil/',
     TWITTER: 'https://twitter.com/yourusername',
-    RESUME: 'https://drive.google.com/file/d/1reQyYLYBYjnXPP2gQvHvCaC5NS4CSbnV/view?usp=sharing',
+    RESUME: 'https://drive.google.com/file/d/1PAtCalM_c6729d5GVCWcoqkqYz-FKsPI/view?usp=sharing',
   }
 } as const;
 
